2c5ea75b42a9c0edd28fcfd7bbf02ed90ab4eedc
dave <davidpalomo2802@gmail.com>
2021-05-18 16:17:57 -0500

    Listando las tareas

--- crud/urls.py
+++ crud/urls.py
@@ -5,8 +5,8 @@ app_name = 'crud_app'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-    # # Page for adding a new task
-    # path('agregar/', views.agregar, name='agregar'),
+    # Page for adding a new task
+    path('agregar/', views.agregar, name='agregar'),
     # path('eliminar/<int:tarea_id>', views.eliminar, name='eliminar'),
     # # Page for editing an entry.
     # path('editar/<int:tarea_id>/', views.editar, name='editar'),
--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render, redirect
 from .models import Tarea
-# from .forms import TareaForm
+from .forms import TareaForm
 
 # Create your views here.
 
@@ -10,4 +10,20 @@ def index(request):
     context = {
         'tareas': tareas
     }
-    return render(request, 'crud/home.html', context)
+    return render(request,'crud/home.html', context)
+
+def agregar(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TareaForm()
+    else:
+        # POST data submitted; process data.
+        form = TareaForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crud_app:index')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'crud/agregar.html', context)
